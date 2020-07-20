d6c186aebf50cdf5ee302197433bdad6f973b4fe
dassepascal <pascaldasse@yahoo.fr>
2020-07-20 10:47:46 +0200

    init

--- php/contact.php
+++ php/contact.php
@@ -3,21 +3,21 @@
     $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "message" => "", "firstnameError" => "", "nameError" => "", "emailError" => "", "phoneError" => "", "messageError" => "", "isSuccess" => false);
     $emailTo = "pascaldasse56@gmail.com";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") 
-    { 
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
         $array["firstname"] = test_input($_POST["firstname"]);
         $array["name"] = test_input($_POST["name"]);
         $array["email"] = test_input($_POST["email"]);
         $array["phone"] = test_input($_POST["phone"]);
         $array["message"] = test_input($_POST["message"]);
-        $array["isSuccess"] = true; 
+        $array["isSuccess"] = true;
         $emailText = "";
-        
+
         if (empty($array["firstname"]))
         {
             $array["firstnameError"] = "Je veux connaitre ton prénom !";
-            $array["isSuccess"] = false; 
-        } 
+            $array["isSuccess"] = false;
+        }
         else
         {
             $emailText .= "Firstname: {$array['firstname']}\n";
@@ -26,18 +26,18 @@
         if (empty($array["name"]))
         {
             $array["nameError"] = "Et oui je veux tout savoir. Même ton nom !";
-            $array["isSuccess"] = false; 
-        } 
+            $array["isSuccess"] = false;
+        }
         else
         {
             $emailText .= "Name: {$array['name']}\n";
         }
 
-        if(!isEmail($array["email"])) 
+        if(!isEmail($array["email"]))
         {
             $array["emailError"] = "T'essaies de me rouler ? C'est pas un email ça  !";
-            $array["isSuccess"] = false; 
-        } 
+            $array["isSuccess"] = false;
+        }
         else
         {
             $emailText .= "Email: {$array['email']}\n";
@@ -46,7 +46,7 @@
         if (!isPhone($array["phone"]))
         {
             $array["phoneError"] = "Que des chiffres et des espaces, stp...";
-            $array["isSuccess"] = false; 
+            $array["isSuccess"] = false;
         }
         else
         {
@@ -56,39 +56,35 @@
         if (empty($array["message"]))
         {
             $array["messageError"] = "Qu'est-ce que tu veux me dire ?";
-            $array["isSuccess"] = false; 
+            $array["isSuccess"] = false;
         }
         else
         {
             $emailText .= "Message: {$array['message']}\n";
         }
-        
-        if($array["isSuccess"]) 
+
+        if($array["isSuccess"])
         {
             $headers = "From: {$array['firstname']} {$array['name']} <{$array['email']}>\r\nReply-To: {$array['email']}";
             mail($emailTo, "Un message de votre site", $emailText, $headers);
         }
-        
+
         echo json_encode($array);
-        
+
     }
 
-    function isEmail($email) 
+    function isEmail($email)
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
-    function isPhone($phone) 
+    function isPhone($phone)
     {
         return preg_match("/^[0-9 ]*$/",$phone);
     }
-    function test_input($data) 
+    function test_input($data)
     {
       $data = trim($data);
       $data = stripslashes($data);
       $data = htmlspecialchars($data);
       return $data;
     }
- 
-?>
-
-
